ff74e05fef5ab96cae24a625c587aa75a11281ab
dibbik <119895965+dibbik@users.noreply.github.com>
2024-06-04 12:38:05 +0300

    Add files via upload

---
+++ Hello/Program.cs
@@ -0,0 +1,19 @@
+﻿string name;
+int age;
+string programCount;
+int money;
+int time;
+bool truth;
+
+name = "Сергей";
+age = 33;
+programCount = "Не первая";
+money = 10;
+time = 100;
+truth = true;
+
+Console.WriteLine("Здравствуйте, меня зовут " + name + ".");
+Console.WriteLine("Мне " + age + "года.");
+Console.WriteLine("Я хочу научиться делать игры. Это моя " +  programCount + " программа на языке C#.");
+Console.WriteLine("Когда я закончу обучение я хочу зарабатывать " +money+ " денег в " + time+ " наносекунд");
+Console.WriteLine("Достигну ли я желаемого? " + truth + "!");
