5834eb54c996c5a79b34af04b35cdd9eb5fd377e
dibbik <119895965+dibbik@users.noreply.github.com>
2024-06-05 12:17:03 +0300

    Add files via upload

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,17 +1,21 @@
-﻿int a = 4 + 6;
-int b = 4 - 1;
-int c = 3 * 7;
-int d = 16 / 8;
-double e = 11.0 / 5;
-int f = 13 / 3;
-int g = 17 % 8;
+﻿string s1 = "Каждый ";
+string s2 = "охотник ";
+string s3 = "желает ";
+string s4 = "знать ";
+string s5 = "где ";
+string s6 = "сидит ";
+string s7 = "фазан.";
 
+Console.Write(s1);
+Console.Write(s2);
+Console.Write(s3);
+Console.Write(s4);
+Console.Write(s5);
+Console.Write(s6);
+Console.WriteLine(s7);
 
+string conkastString = s1+s2+s3+s4+s5+s6+s7;
+Console.WriteLine(conkastString);
 
-Console.WriteLine("4 плюс 6 = " + a);
-Console.WriteLine("4 уменьшенное на 1 = " + b);
-Console.WriteLine("3 умножить на 7 = " + c);
-Console.WriteLine("16 разделить на 8 = " + d);
-Console.WriteLine("11 разделить на 5 = " + e);
-Console.WriteLine("Целая часть от деления 13 на 3 = " + f);
-Console.WriteLine("Остаток от деления 17 на 8 = " + g);
+string interpolatedString = $"{s1}{s2}{s3}{s4}{s5}{s6}{s7}";
+Console.WriteLine(interpolatedString);
