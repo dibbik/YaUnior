a440693464f87f3ea8edd3a19f720187948a8cf2
dibbik <119895965+dibbik@users.noreply.github.com>
2024-06-05 12:58:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int magicNumber = 4;
+
+Console.WriteLine("Угадай число!");
+Console.WriteLine();
+Console.WriteLine("Загадо число от 1 до 5. Попробуйте его угадать!");
+Console.WriteLine();
+Console.Write("Введите ваше число: ");
+
+int input = Convert.ToInt32(Console.ReadLine());
+
+if (input == magicNumber)
+
+    Console.WriteLine($"Да! Это число {magicNumber}!");
+
+else
+    Console.WriteLine($"Нет! Это не {input}!");
+    
