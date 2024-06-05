8809cf8999283955f1320a4006337317cfd81b24
dibbik <119895965+dibbik@users.noreply.github.com>
2024-06-05 12:27:30 +0300

    Add files via upload

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,21 +1,7 @@
-﻿string s1 = "Каждый ";
-string s2 = "охотник ";
-string s3 = "желает ";
-string s4 = "знать ";
-string s5 = "где ";
-string s6 = "сидит ";
-string s7 = "фазан.";
+﻿//а вы смотрите код?)))
 
-Console.Write(s1);
-Console.Write(s2);
-Console.Write(s3);
-Console.Write(s4);
-Console.Write(s5);
-Console.Write(s6);
-Console.WriteLine(s7);
+string strinNumber = "256";
+int numberFromString = Convert.ToInt32(strinNumber);
+int square = numberFromString * numberFromString;
 
-string conkastString = s1+s2+s3+s4+s5+s6+s7;
-Console.WriteLine(conkastString);
-
-string interpolatedString = $"{s1}{s2}{s3}{s4}{s5}{s6}{s7}";
-Console.WriteLine(interpolatedString);
+Console.WriteLine("Квадрат введенного числа равен " + square);
